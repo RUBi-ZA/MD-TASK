eea596899b6cb667833beda312c2e5fcf4401bd2
David Brown <davidbrownza@outlook.com>
2017-09-30 14:02:21 +0200

    Specify package versions to avoid incompatible updates

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@ virtualenv venv
 source venv/bin/activate
 
 pip install --upgrade pip
-pip install numpy
-pip install scipy
-pip install matplotlib cython networkx natsort pandas
-pip install mdtraj
+pip install numpy==1.13.3
+pip install scipy==0.19.1
+pip install matplotlib==2.0.2
+pip install cython==0.27 
+pip install networkx==1.11
+pip install natsort==5.1.0
+pip install pandas==0.20.3
+pip install mdtraj==1.9.1
