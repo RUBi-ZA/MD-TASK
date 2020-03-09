2940b5c21c1af55fd98ee2883ce2afdffa3a606b
Michael <mickeyjhg@gmail.com>
2020-03-09 11:38:04 +0200

    Downgrade networkx
    Latest version of networkx was causing an error:
    'generator' object is not subscriptable

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ pip3 install numpy==1.18.1
 pip3 install scipy==1.4.1
 pip3 install matplotlib==3.2.0
 pip3 install cython==0.29.15 
-pip3 install networkx==2.4
+pip3 install networkx==1.11
 pip3 install natsort==7.0.1
 pip3 install pandas==0.25.3
 pip3 install mdtraj==1.9.3
