58e7d555bb926178c3c535b90565bc755bc2a7cb
michaelglenister <mickeyjhg@gmail.com>
2021-02-02 11:15:57 +0200

    Update calc_correlation.py

--- calc_correlation.py
+++ calc_correlation.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+"""
+Calculates Dynamic Cross Correlations from MD data
+"""
 #
 # Calculate correlation in MD trajectory
 #
@@ -6,159 +9,148 @@
 #
 # Author: Caroline Ross
 # Date: 17-11-2016
+__version__ = 1.1
+__date__ = "15th November 2020"
 
-from matplotlib import cm
-
+import math
+import argparse
 import numpy as np
-
+import matplotlib
+from matplotlib import cm
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from lib.cli import CLI
 from lib.utils import Logger
 from lib.trajectory import load_trajectory
 
-import argparse, math, matplotlib
-
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-
-
-def parse_traj(traj, topology=None, step=1, selected_atoms=["CA"], lazy_load=False):
+def parse_traj(traj, topology=None, step=1, selected_atoms="CA",
+               lazy_load=False):
+    """Reads a trajectory and returns a dictionary of residues with their
+    coordinates from each frame reshaped as nested lists"""
+    selected_atoms =  selected_atoms.strip().split(",")
     traj = load_trajectory(traj, topology, step, lazy_load)[0]
-
     residues = {}
-
     for frame in traj:
         for atom in frame.topology.atoms:
             if atom.name in selected_atoms:
-                res = atom.residue.resSeq
-
+                res = atom.residue.index
                 ac = frame.xyz[0, atom.index]
                 co_ords = [ac[0], ac[1], ac[2]]
-
                 if res in residues:
                     residues[res].append(co_ords)
                 else:
-                    residues[res] =  [co_ords]
-
+                    residues[res] = [co_ords]
     return residues
 
-
-
 def mean_dot(m1, m2, size):
+    """Computes the pairwise dot product between two arrays of deviations
+    for all MD frames, before returning the averages"""
     DOT = np.zeros(size)
-
     for t in range(size):
-        DOT[t] = np.dot(m1[t],m2[t])
-
+        DOT[t] = np.dot(m1[t], m2[t])
     return np.mean(DOT)
 
-
-
 def correlate(residues):
-
+    """Calculates the DCC matrix"""
     sorted_residues = sorted(residues.keys())
-
     num_trajectories = len(residues[sorted_residues[0]])
     num_residues = len(residues)
-
-    correlation = np.zeros((num_residues, num_residues))
-
+    correlation = np.ones((num_residues, num_residues))
     for a, key_a in enumerate(sorted_residues):
         i = residues[key_a]
         resI = np.array(i)
-        meanI = np.tile((np.mean(resI, 0)),(num_trajectories, 1))
-        idelta = resI - meanI;
+        meanI = np.tile((np.mean(resI, 0)), (num_trajectories, 1))
+        idelta = resI - meanI
         magnitudeI = math.sqrt(mean_dot(idelta, idelta, num_trajectories))
-
         for b, key_b in enumerate(sorted_residues):
-            j = residues[key_b]
-            resJ = np.array(j)
-            meanJ = np.tile((np.mean(resJ, 0)),(num_trajectories, 1))
-            jdelta = resJ - meanJ
-            magnitudeJ = math.sqrt(mean_dot(jdelta, jdelta, num_trajectories))
-
-            meanDotIJ = mean_dot(idelta, jdelta, num_trajectories)
-            magProd = magnitudeI * magnitudeJ
-
-            correlation[a,b] = meanDotIJ/magProd
-
+            if a < b:
+                j = residues[key_b]
+                resJ = np.array(j)
+                meanJ = np.tile((np.mean(resJ, 0)),(num_trajectories, 1))
+                jdelta = resJ - meanJ
+                magnitudeJ = math.sqrt(mean_dot(jdelta, jdelta, num_trajectories))
+                meanDotIJ = mean_dot(idelta, jdelta, num_trajectories)
+                magProd = magnitudeI * magnitudeJ
+                corr = meanDotIJ/magProd
+                correlation[a, b] = corr
+                correlation[b, a] = corr
     return correlation
 
-
 def plot_map(correlation, title, output_prefix):
+    """Plots and saves the heat map"""
     M = np.array(correlation)
-
     ax = plt.subplots()[1]
-    colors = [('white')] + [(cm.jet(i)) for i in range(40,250)]
-
-    new_map = matplotlib.colors.LinearSegmentedColormap.from_list('new_map', colors, N=300)
-    heatmap = ax.pcolor(M, cmap=new_map, vmin=-1, vmax=1)
-
+    heatmap = ax.pcolor(M, cmap="RdBu_r", vmin=-1, vmax=1)
     fig = plt.gcf()
     ax.set_frame_on(False)
     ax.grid(False)
-
     plt.xticks(rotation=90)
-
-    # Turn off all the ticks
     ax = plt.gca()
+    ax.set_xticks(range(0, correlation.shape[0], 10))
+    ax.set_yticks(range(0, correlation.shape[1], 10))
     for t in ax.xaxis.get_major_ticks():
         t.tick1line.set_visible = False
         t.tick2line.set_visible = False
-        t.label.set_fontsize(8)
-
+        #t.label.set_fontsize(8)
     for t in ax.yaxis.get_major_ticks():
         t.tick1line.set_visible = False
         t.tick2line.set_visible = False
-        t.label.set_fontsize(8)
-
-    plt.title(title, fontsize=16)
-    plt.xlabel('Residue Index', fontsize=12)
-    plt.ylabel("Residue Index", fontsize=12)
-
+        #t.label.set_fontsize(8)
+    plt.title(title, fontsize=10)
+    plt.xlabel('Residue index')
+    plt.ylabel("Residue index")
     plt.colorbar(heatmap, orientation="vertical")
-    plt.savefig('%s.png' % output_prefix, dpi=300)
+    plt.savefig('%s.png' % output_prefix, bbox_inches="tight", dpi=300)
     plt.close('all')
 
-
-
-def print_correlation(correlation, output_prefix):
+def write_correlation(correlation, output_prefix):
+    """Saves the correlation matrix"""
     with open("%s.txt" % output_prefix, "w") as w:
         rows = correlation.shape[0]
         cols = correlation.shape[1]
-
         for r in range(rows):
             for c in range(cols):
                 w.write('%s ' % str(correlation[r,c]))
             w.write('\n')
 
-
-
 def main(args):
+    """The main function"""
     log.info("Preparing a trajectory matrix...\n")
-    traj_matrix = parse_traj(args.trajectory, args.topology, args.step, lazy_load=args.lazy_load)
-
+    traj_matrix = parse_traj(args.trajectory, args.topology, args.step,
+            lazy_load=args.lazy_load, selected_atoms=args.select_atoms)
     log.info("Correlating...\n")
     correlation = correlate(traj_matrix)
-
+    plt.rcParams["font.family"] = "serif"
+    plt.rcParams["xtick.labelsize"] = 7
+    plt.rcParams["ytick.labelsize"] = 7
     log.info("Plotting heat map...\n")
     plot_map(correlation, args.title, args.prefix)
-    print_correlation(correlation, args.prefix)
-
+    write_correlation(correlation, args.prefix)
 
 log = Logger()
 
 if __name__ == "__main__":
-
-    #parse cmd arguments
-    parser = argparse.ArgumentParser()
-
-    #custom arguments
-    parser.add_argument("--trajectory", help="Trajectory file")
-    parser.add_argument("--topology", help="Referencce PDB file (must contain the same number of atoms as the trajectory)")
-    parser.add_argument("--step", help="Size of the step to take when iterating the the trajectory frames", type=int)
-    parser.add_argument("--lazy-load", help="Iterate through trajectory, loading one frame into memory at a time (memory-efficient for large trajectories)", action='store_true', default=False)
-
+    parser = argparse.ArgumentParser(
+            description="Calculates Dynamic Cross Correlation from MD data")
+    parser.add_argument("trajectory", help="Trajectory file")
+    parser.add_argument("--topology",
+                        help="Referencce PDB file (must contain \
+                        the same number of atoms as the trajectory)")
+    parser.add_argument("--step",
+                        help="Size of the step to take when iterating \
+                        the the trajectory frames", type=int)
+    parser.add_argument("--select_atoms",
+                        help="Comma-separated list of atoms types \
+			(without spaces) to use for DCC calculation. \
+			E.g. 'CA,P'. The backbone phosphorus 'P' atom \
+			can be a good choice for representing nucleotides.\
+			(default=CA)",
+                        type=str, default="CA")
+    parser.add_argument("--lazy-load", action='store_true', default=False,
+                        help="Iterate through trajectory, loading one \
+                        frame into memory at a time (memory-efficient \
+                        for large trajectories)")
     parser.add_argument("--title", help="Title for heatmap", default="Protein")
-    parser.add_argument("--prefix", help="Prefix for output files", default="correlation")
-
+    parser.add_argument("--prefix", default="correlation",
+                        help="Prefix for output files")
     CLI(parser, main, log)
