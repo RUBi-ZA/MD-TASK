35fc9716fae5b4480ca69723151ec1419b745a42
michaelglenister <mickeyjhg@gmail.com>
2021-02-02 19:13:12 +0200

    CI test

--- test/test_L.sh
+++ test/test_L.sh
@@ -19,7 +19,7 @@ python $BIN_DIR/calc_network.py --topology $PREFIX.pdb --threshold 7.0 --step 10
 
 mv ${PREFIX}_0_L.dat ref_${PREFIX}_L.dat
 
-python $BIN_DIR/calc_delta_L.py --generate-plots --normalize --normalization-mode ${NORM} --reference ref_${PREFIX}_avg_L.dat --alternatives ${PREFIX}_*_L.dat
+python $BIN_DIR/calc_delta_L.py --generate-plots --normalize --normalization-mode ${NORM} --reference ref_${PREFIX}_L.dat --alternatives ${PREFIX}_*_L.dat
 python $BIN_DIR/avg_network.py --data ${PREFIX}_*_L_${NORM}_delta_L.dat --data-type delta-L --prefix ${PREFIX} --generate-plots --x-label "Residues" --y-label "Avg delta L" --title "Wild Type"
 
 
@@ -43,6 +43,10 @@ echo ""
 echo "#### AVERAGE SHORTEST PATH - WT vs MUTANT ####"
 echo ""
 ls
-python $BIN_DIR/compare_networks.py --prefix "wt_mutant_L_avg" --reference-label Wild-type --alternative-label Mutant --y-label "Delta L" --reference wt_delta_L_avg.dat --alternative mutant_delta_L_avg.dat
-python $BIN_DIR/compare_networks.py --prefix "wt_mutant_L_std_dev" --reference-label Wild-type --alternative-label Mutant --y-label "Delta L" --reference wt_delta_L_std_dev.dat --alternative mutant_delta_L_std_dev.dat
+#python $BIN_DIR/compare_networks.py --prefix "wt_mutant_L_avg" --reference-label Wild-type --alternative-label Mutant --y-label "Delta L" --reference wt_delta_L_avg.dat --alternative mutant_delta_L_avg.dat
+#python $BIN_DIR/compare_networks.py --prefix "wt_mutant_L_std_dev" --reference-label Wild-type --alternative-label Mutant --y-label "Delta L" --reference wt_delta_L_std_dev.dat --alternative mutant_delta_L_std_dev.dat
+#python $BIN_DIR/delta_networks.py --reference wt_delta_L_avg.dat --reference-std wt_delta_L_std_dev.dat --alternatives *delta_L_avg.dat --alternatives-std *delta_L_std_dev.dat --absolute --prefix wt_mutant_cmap --title "My Protein" --x-label "Residues" --y-label "Proteins"
+
+python $BIN_DIR/compare_networks.py --prefix "wt_mutant_L_avg" --reference-label Wild-type --alternative-label Mutant --y-label "Delta L" --reference ref_wt_L.dat --alternative mutant_delta_L_avg.dat
+python $BIN_DIR/compare_networks.py --prefix "wt_mutant_L_std_dev" --reference-label Wild-type --alternative-label Mutant --y-label "Delta L" --reference ref_wt_L.dat --alternative mutant_delta_L_std_dev.dat
 python $BIN_DIR/delta_networks.py --reference wt_delta_L_avg.dat --reference-std wt_delta_L_std_dev.dat --alternatives *delta_L_avg.dat --alternatives-std *delta_L_std_dev.dat --absolute --prefix wt_mutant_cmap --title "My Protein" --x-label "Residues" --y-label "Proteins"
