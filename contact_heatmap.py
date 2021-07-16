d1c0dff8a6acaf81add3cd980bd008c67ee1ee84
olivier <oliserand@gmail.com>
2021-07-16 15:09:55 +0200

    chm update

--- contact_heatmap.py
+++ contact_heatmap.py
@@ -5,15 +5,16 @@ Compare multiple weighted contact heatmap around a common residue position
 __author__ = "Olivier Sheik Amamuddy"
 __copyright__ = "Copyright 2020, Research Unit in Bioinformatics"
 __license__ = "GNU GPL 3.0"
-__version__ = "1.3.2"
+__version__ = "1.3.5"
 __email__ = "oliserand@gmail.com"
-__status__ = "Production"
-__date__ = "15th December 2020"
+__status__ = "Development"
+__date__ = "16th July 2021"
 
 import os
 import sys
 import argparse
 from datetime import datetime
+from collections import OrderedDict as dict
 import pandas as pd
 import matplotlib
 matplotlib.use('Agg')
@@ -21,16 +22,44 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 from lib.utils import format_seconds
 
+def fold(df, kind):
+    """Folds an axis if duplicates residues and/or chains are present
+    Assumes columns are ordered
+    """
+    df = df.transpose()
+    residues = [i.split(".")[0] for i in df.columns]
+    newdataset = dict()
+    if kind == "chain":
+        for i, char in enumerate(residues):
+            # Ignores chain label, merges any successive residues,
+            # and adds anything that's unique
+            # if dealing with a mutant do not fold
+            if char not in newdataset:
+                newdataset[char] = df.iloc[:,i]
+            else:
+                newdataset[char] += df.iloc[:,i]
+    elif kind == "res":
+        for i, char in enumerate(residues):
+            # Ignores chain label, merges any successive residues,
+            # and adds anything that's unique
+            # if dealing with a mutant, fold
+            if char[3:] not in newdataset:
+                newdataset[char] = df.iloc[:,i]
+            else:
+                newdataset[char] += df.iloc[:,i]
+    return pd.DataFrame(newdataset).transpose()
+
 def plot_heatmap(allnetworks_copy, outfilename, xlabel='Target residue',
-                ylabel='Contacting residues', figwidth=16,
+                ylabel='Contacting residues', figwidth=16, annot_size=6,
                 figheight=16, fontsize=12, dpi=450, annotate=True,
                 shorten_yticks=False, xtickfontsize=6, ytickfontsize=6):
-    """Plots the heat map"""
+    """Plots the heat map and returns prepared dataset"""
     plt.figure(figsize=[figwidth, figheight])
-    axes = sns.heatmap(data=allnetworks_copy.transpose(), cmap='hot_r',
+    data = allnetworks_copy.transpose()
+    axes = sns.heatmap(data=data, cmap='hot_r',
                        annot=annotate, linecolor="lightgrey",
                        linewidth=0.5, xticklabels=1, yticklabels=1,
-                       annot_kws={"rotation":90})
+                       annot_kws={"rotation":90, "size":annot_size})
     axes.set_yticklabels(axes.get_yticklabels(), rotation=0, fontsize=ytickfontsize)
     axes.set_xticklabels(axes.get_xticklabels(), rotation=90, fontsize=xtickfontsize)
     axes.set_ylabel(xlabel, fontsize=fontsize)
@@ -40,15 +69,21 @@ def plot_heatmap(allnetworks_copy, outfilename, xlabel='Target residue',
         axes.set_yticklabels(new_labels)
     plt.tight_layout()
     plt.savefig(outfilename, dpi=dpi, bbox_inches="tight")
+    return data
+
+def write_csv(data, outfilename):
+    """Saves a dataframe, with repoting"""
+    data.to_csv(outfilename)
+    log("INFO: Wrote {}\n".format(outfilename))
 
 def main(arg):
     """Script main"""
     networks        = arg.networks
     outfilename     = arg.output
-    allnetworks     = {}
+    allnetworks     = dict()
     common_residues = set()
     plt.rcParams["font.family"] = "serif"
-    for networkfile in networks:
+    for networkfile in sorted(networks):
         try:
             variant = pd.read_csv(networkfile, header=None,
                     names=['source','dest','weight'])
@@ -63,7 +98,7 @@ def main(arg):
     common_residue_order = pd.Series(
             [int(i[3:-2]) for i in common_residues]).sort_values(ascending=True).index
     common_residues = pd.Series(list(common_residues)).iloc[common_residue_order].values
-    allnetworks_copy = pd.DataFrame(data=0,columns=allnetworks.keys(),
+    allnetworks_copy = pd.DataFrame(data=0, columns=allnetworks.keys(),
                                     index=common_residues)
     for center in allnetworks:
         for common_residue in common_residues:
@@ -71,10 +106,14 @@ def main(arg):
                 allnetworks_copy.loc[common_residue, center] = \
                         allnetworks[center].loc[
                                 allnetworks[center].dest == common_residue, 'weight'].values
-    plot_heatmap(allnetworks_copy, outfilename, figheight=arg.figheight,
+    if arg.fold:
+        allnetworks_copy = fold(allnetworks_copy, kind=args.fold)
+    data = plot_heatmap(allnetworks_copy, outfilename, figheight=arg.figheight,
                  figwidth=arg.figwidth, fontsize=arg.fontsize, dpi=arg.dpi,
                  annotate=args.annotate, shorten_yticks=args.shorten_yticks,
-                 xtickfontsize=args.xtickfontsize, ytickfontsize=args.ytickfontsize)
+                 xtickfontsize=args.xtickfontsize,
+                 ytickfontsize=args.ytickfontsize)
+    write_csv(data, "{}.csv".format(outfilename[:-4]))
 
 def check_filename(filename):
     """Sanity check for filenames"""
@@ -121,6 +160,9 @@ def parse_args():
     parser.add_argument("--shorten_yticks", action="store_true",
                         help="Shorten y tick labels by ignoring file names\
                         (default:no)")
+    parser.add_argument("--fold", type=str, default=False,
+                        help="Fold the x-axis by residue or chain Options:\
+                         [res|chain] (default:no). Use with caution")
     arguments = parser.parse_args()
     return arguments
 
